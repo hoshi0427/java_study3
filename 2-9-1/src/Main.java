c5b411946ad5894ac720fa1e347ed4204be96e4d
Hoshi0427 <m.hoshi@micsolutions.co.jp>
2019-02-08 11:04:25 +0900

    [2-9-1]テキストファイルの書き込み

--- 2-9-1/src/Main.java
+++ 2-9-1/src/Main.java
@@ -1,8 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileWriter fw = new FileWriter("C:\\git\\java_study3\\2-9-1\\rpgsave.dat", true);
 
+		fw.write("Test");
+		fw.flush();
+		fw.close();
 	}
 
 }
