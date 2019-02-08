eb6431051e1158010034665f8fe8d3cb164de272
Hoshi0427 <m.hoshi@micsolutions.co.jp>
2019-02-08 11:25:47 +0900

    [2-9-2]テキストファイルの読み込み

--- 2-9-1/src/Main.java
+++ 2-9-1/src/Main.java
@@ -1,14 +1,24 @@
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		FileWriter fw = new FileWriter("C:\\git\\java_study3\\2-9-1\\rpgsave.dat", true);
+//		FileWriter fw = new FileWriter("C:\\git\\java_study3\\2-9-1\\rpgsave.dat", true);
 
-		fw.write("Test");
-		fw.flush();
-		fw.close();
+//		fw.write("Test");
+//		fw.flush();
+//		fw.close();
+
+		FileReader fr = new FileReader("rpgsave.dat");
+		System.out.println("全てのデータを読んで表示します。");
+		int i = fr.read();
+		while(i != -1) {
+			char c = (char) i;
+			System.out.print(c);
+			i = fr.read();
+		}
+		System.out.println("ファイルの末尾に到着しました");
+		fr.close();
 	}
-
 }
